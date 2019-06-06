99d7e20ce8332fe574a226bf91fdb7562cf75731
Stig B. Dørmænen <stigbd@gmail.com>
2019-06-06 19:13:33 +0200

    Improve console logging

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -56,39 +56,40 @@ var config = {
 
 var handleMessage = function(payload) {
 
-       if (payload.body.dataOK) {
-           // if the data in the payload is good, lets delete the message from the queue
-           console.log('payload received: ' + payload.body.msg);
-           payload.ack();
-       } else {
-           // the data is not good, lets move the message to the dead-letter queue
-           console.log('rejecting message: ' + payload.body.msg );
-           payload.reject();
-       }
+  var body = JSON.stringify(payload.body, null, 2);
+  if (payload.body.dataOK) {
+    // if the data in the payload is good, lets delete the message from the queue
+    console.log('payload received: ' + body);
+    payload.ack();
+  } else {
+    // the data is not good, lets move the message to the dead-letter queue
+    console.log('rejecting message: ' + body);
+    payload.reject();
+  }
 
-   };
+};
 
 var startListening = function() {
 
-    rabbit.handle({}, handleMessage);
+  rabbit.handle({}, handleMessage);
 
-    // must define handler before starting the subscription, otherwise messages will be lost
-    rabbit.startSubscription(config.queues[0].name, config.connection.name);
+  // must define handler before starting the subscription, otherwise messages will be lost
+  rabbit.startSubscription(config.queues[0].name, config.connection.name);
 };
 
 // Open a connection and a channel, declare the queue
 rabbit
-    .configure(config)
-    .then( function() {
-        console.log('rabbit is hopping');
-        // ready to start rabbit receivers and publishers
-        startListening();
-    })
-    .then(null, function(err) {
-        console.log('Error on configuring rabbit: ', err.stack);
-        process.exit(1);
-    })
-    .catch( function(err) {
-      console.log(err.stack);
-      process.exit(1);
-    });
+.configure(config)
+.then( function() {
+  console.log('rabbit is hopping');
+  // ready to start rabbit receivers and publishers
+  startListening();
+})
+.then(null, function(err) {
+  console.log('Error on configuring rabbit: ', err.stack);
+  process.exit(1);
+})
+.catch( function(err) {
+  console.log(err.stack);
+  process.exit(1);
+});
